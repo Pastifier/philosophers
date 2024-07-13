b5d32ccb1c417023b1e7e8a0058d5235f01205c7
Emran Binjamaan <therealemran.design@gmail.com>
2024-07-13 05:41:03 +0400

    TODO

--- philo/main.c
+++ philo/main.c
@@ -75,6 +75,7 @@ static bool	init_philos(int philo_count, t_philo **philos, t_data *context,
 			(*philos)[i].eat_count = -1;
 		if (pthread_mutex_init(&(*philos)[i].left_mutex, NULL))
 			return (write_error(MUTEX_INIT_FAILED), free(*philos), false);
+		// needs checking
 		(*philos)[i].right_mutex = &context->forks[i].mutex;
 		i++;
 	}
