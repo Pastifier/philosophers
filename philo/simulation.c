b48ab485ef2b40baf791c5c8316b2116afeaf93a
Emran Binjamaan <therealemran.design@gmail.com>
2024-07-18 02:24:16 +0400

    Added first draft of philo_eat() function to be used in the routine.

--- philo/simulation.c
+++ philo/simulation.c
@@ -6,11 +6,14 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 18:06:16 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/07/13 04:55:40 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/07/18 02:15:45 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <unistd.h>
+
+static void philo_eat(t_philo *philo);
 
 int	start_sim(t_data *context, t_philo *philos)
 {
@@ -20,4 +23,40 @@ int	start_sim(t_data *context, t_philo *philos)
 	//	return (/*log=THREAD_FAILED*/EXIT_FAILURE);
 	(void)philos; (void)context;
 	return (1);
+}
+
+void	routine(t_philo *philo)
+{
+	while (true)
+	{
+		pthread_mutex_lock(&philo->context->death_mutex);
+		if (philo->context->death_flag)
+		{
+			pthread_mutex_unlock(&philo->context->death_mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->context->death_mutex);
+		philo_eat(philo);
+	}
+}
+
+static void philo_eat(t_philo *philo)
+{
+	if (philo->id % 2)
+		usleep(100);
+	pthread_mutex_lock(&philo->left_mutex);
+	print_philo_status(philo, MSG_FORK);
+	if (philo->right_mutex)
+	{
+		pthread_mutex_lock(philo->right_mutex);
+		print_philo_status(philo, MSG_FORK);
+		philo->last_eat = my_gettime();
+		philo->eat_count++;
+		print_philo_status(philo, MSG_EATING);
+		my_usleep(philo->time_to_eat, philo->context);
+		pthread_mutex_unlock(philo->right_mutex);
+		pthread_mutex_unlock(&philo->left_mutex);
+	}
+	else
+		pthread_mutex_unlock(&philo->left_mutex);
 }
