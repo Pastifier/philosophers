a9a72551567e5ba4747e738abd36c9a89f50b90d
Emran Binjamaan <therealemran.design@gmail.com>
2024-07-20 04:50:11 +0400

    init_threads()

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 05:12:42 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/07/13 05:28:00 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/07/19 06:22:37 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,19 @@ bool	init_philo_attr(t_philo *const *philos, t_data *context, char *argv[],
 	if (fetcher.error || fetcher.value < 0)
 		return (write_error(INVALID_VALUE), false);
 	(*philos)[i].time_to_sleep = fetcher.value;
+	return (true);
+}
+
+bool	init_threads(t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos->context->philo_count)
+	{
+		if (pthread_create(&philos[i].thread, NULL, routine, &philos[i]))
+			return (write_error(THREAD_FAILED), false);
+		i++;
+	}
 	return (true);
 }
