e472465a818ec23dd12d47ee62911937404a5113
Emran Binjamaan <therealemran.design@gmail.com>
2024-08-10 00:22:32 +0400

    From scratch!

---
+++ philo/simulation.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simulation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 00:19:12 by ebinjama          #+#    #+#             */
+/*   Updated: 2024/08/10 00:21:38 by ebinjama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+bool	simulation(t_data *data)
+{
+	((void)data);
+	return (true);
+}
+
+void	*routine(void *phcontext)
+{
+	((void)phcontext);
+	return (NULL);
+}
